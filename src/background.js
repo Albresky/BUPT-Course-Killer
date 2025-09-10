e00300386d0f808e8ea1700e69c4e6722e451b78
Shi Kai <albre02@outlook.com>
2025-09-10 22:21:47 +0800

    [fix] Add IPset of servers

--- src/background.js
+++ src/background.js
@@ -2,14 +2,16 @@ chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
         var url = details.url;
         console.log('url: ' + url);
-
-        if (url === "http://10.3.255.31/js/lhgdialog/lhgdialog.min.js?self=true") {
+        
+        var ip = url.split('/')[2];
+        const allowedIPs = new Set(["10.3.255.30", "10.3.255.31", "10.3.255.32", "10.3.255.33"]);
+        if (allowedIPs.has(ip) && url.includes("/js/lhgdialog/lhgdialog.min.js?self=true")) {
             console.log(`Redirecting: ${url} => ${chrome.extension.getURL('scripts/lhgdialog.min.js')}`);
             return { redirectUrl: chrome.extension.getURL('scripts/lhgdialog.min.js') };
         }
 
         return { redirectUrl: url };
     },
-    { urls: ["*://10.3.255.31/*"] },
+    { urls: ["*://10.3.255.30/*", "*://10.3.255.31/*", "*://10.3.255.32/*", "*://10.3.255.33/*"] },
     ["blocking"]
 );
